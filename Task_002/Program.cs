ba4db0598e319e178c74414f340b2ee0142e0208
Ирина <ira.sheina.92@gmail.com>
2023-11-17 10:21:58 +0300

    Добавлено решение Task_003

---
+++ Task_002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+    
+} 
+  
+void PrintSumNumbers(int M, int N)
+{
+    int sum = 0;
+
+    for (int i = M; i <= N; i++)
+    {
+         sum += i;
+    }
+    
+    Console.WriteLine("Сумма натуральных чисел: " + sum);
+}
+
+//.......................................................
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+PrintSumNumbers(M, N);
+
+
---
+++ Task_003/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+} 
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write("Значение функции Аккермана для введенных чисел = " + Akkerman(m, n)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+// ....................................
+int m = ReadInt("Введите число m: ");
+int n = ReadInt("Введите число n: ");
+AkkermanFunction(m, n);
